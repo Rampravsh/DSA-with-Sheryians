8c0382ee720883f5cd2e52194d346f2ccbc39dbf
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-21 01:15:39 +0530

    create two function for geting sum of value in given range first function crating prefixSum and second one for Queries to get result in this method we have not to call prefixSum for every queries and we not using class

--- Prefix Sum/index.js
+++ Prefix Sum/index.js
@@ -17,3 +17,22 @@
 //❓❓❓ Given an array of integers of size n. Answer q queries where you need to print the sum
 //  of values in a given range of indices from 1 to r (both included).
 // 📝📝📝 The values of 1 and r in queries follow 1-based indexing.
+let arr = [2, 3, 4, 2, 5, 1, 19];
+
+function prefixSum(arr) {
+  let n = arr.length;
+  let res = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; i++) {
+    res[i + 1] = res[i] + arr[i];
+  }
+  return res;
+}
+
+let prefixSumArr = prefixSum(arr);
+console.log(prefixSumArr);
+
+function sumValueOf(l, r, arr = prefixSumArr) {
+  return arr[r] - arr[l - 1];
+}
+
+console.log(sumValueOf(2, 4));
